85fb70ad76316c716f47f386356f1b7a60d3f290
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-08 11:16:06 +0300

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,12 @@
-let state = { count: 0 };
-
-function changeState(state, action) {
+let state;
+// starts off undefined
+function changeState(state = { count: 0 }, action){
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
-    default:
-      return state;
+
+      default:
+        return state;
   }
 }
 
@@ -15,6 +16,8 @@ function dispatch(action) {
 }
 
 function render() {
-  const app = document.querySelector("#app");
-  app.textContent = state.count;
+  document.body.textContent = state.count;
 }
+
+dispatch({ type: "@@INIT" });
+// passes through our initial action
